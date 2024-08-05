b2e304fae8e286f3661e4fb9a60a7f9f7411fdd4
Jisha1994 <pdjisha1994@gmail.com>
2024-08-05 12:09:05 +0530

    sort

---
+++ sorting.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class sorting {
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 8, 1, 3};
+
+      
+        Arrays.sort(numbers);
+
+       
+        System.out.println("Sorted array: " + Arrays.toString(numbers));
+    }
+}
+
+    
