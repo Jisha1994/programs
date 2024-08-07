5229bdb2dedb33c23f22e10c51b6c02df1232a55
Jisha1994 <pdjisha1994@gmail.com>
2024-08-07 10:06:36 +0530

    hi

--- vote.java
+++
@@ -1,9 +0,0 @@
-public class vote{
-    public static void main(String [] args){
-      int age;
-      if(age>=18)  
-      System.out.println("Eligible to Vote");
-      else
-      System.out.println("Not Eligible to Vote");
-    }
-}
